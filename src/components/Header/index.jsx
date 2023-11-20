6addcc0d35a18bfb0639e9be36ab5e89616b2dbc
SabinaMB <sabina.marian.blaariu@gmail.com>
2023-11-20 11:28:50 +0100

    prevent empty submit

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,6 +7,7 @@ import { useState } from 'react';
 //prop handleAddTask for handling the addition of new tasks
 export function Header({ handleAddTask}) {
   const [title, setTitle] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
 
   function handleSubmit(event) {
@@ -14,10 +15,21 @@ export function Header({ handleAddTask}) {
 
     handleAddTask(title);
     setTitle('');
+
+      // Check if the title is not empty before adding the task
+      if (title.trim() !== '') {
+        handleAddTask(title);
+        setTitle('');
+        setErrorMessage('');
+      } else {
+        //provide feedback to the user about the empty submission
+        setErrorMessage('Enter a book title before adding.');
+      }
   }
 
   function onChangeTitle(event) {
     setTitle(event.target.value);
+    setErrorMessage('');
   }
 
   return (
@@ -25,7 +37,9 @@ export function Header({ handleAddTask}) {
      <h1>Books to read</h1>
       <form onSubmit={handleSubmit} className={styles.newTaskForm}>
         <input placeholder="Add a new book" type="text" onChange={onChangeTitle} value={title} />
+        {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>} 
         <button>Add<AiOutlinePlusCircle size={20} /></button>
+         
       </form>
     </header>
   )
